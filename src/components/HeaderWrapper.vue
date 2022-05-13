c21a42d06f00e24964af8ca955e388c19fb5d8ab
pumba-dev <pumbadeveloper@gmail.com>
2022-05-13 15:26:10 -0300

    Header Created

---
+++ src/components/HeaderWrapper.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <div class="header__inner-container">
+      <a href="https://perfectpay.com.br/" target="_blank" class="header__title"
+        >Perfect Pay</a
+      >
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+@import "@/sass/variables.scss";
+
+.header {
+  padding: 30px 0;
+  margin-bottom: 25px;
+  background-color: #fff;
+
+  .header__inner-container {
+    max-width: 1000px;
+    margin: auto;
+  }
+
+  .header__title {
+    text-decoration: none;
+    font-weight: 700;
+    font-size: $font-3xl;
+  }
+}
+</style>
