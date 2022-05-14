6b8c3a81a31c9d8fbb28cdfdc7e1651398916574
pumba-dev <pumbadeveloper@gmail.com>
2022-05-14 20:25:44 -0300

    Market List Componentization

---
+++ src/components/MarketList.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="market-list">
+    <MarketItem
+      v-for="item in itemList"
+      :key="item.id"
+      :item="item"
+    ></MarketItem>
+  </section>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import MarketItem from "./MarketItem.vue";
+
+defineProps({
+  itemList: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/sass/variables.scss";
+@import "@/sass/media-querys.scss";
+
+.market-list {
+  display: grid;
+  grid-gap: 10px 10px;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+
+  @include media-query(sm) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+  @include media-query(md) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+}
+</style>
